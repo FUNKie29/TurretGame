0273a71dcf5bb644ef46f7631a3b3332986db964
Nathan <nathan.hansen429@gmail.com>
2023-10-15 18:18:09 -0400

    Player Movement
    Player can move, and place down a turret

---
+++ Turrets n Shit/Assets/PlayerMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float movementSpeed;
+
+    [SerializeField]
+    private Rigidbody2D rb;
+
+    [SerializeField]
+    private Object turretObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f);
+
+        rb.transform.position += movement * movementSpeed * Time.deltaTime;
+
+        if (Input.GetKeyDown("space")){
+            CreateTurret();
+        }
+    }
+
+    private void CreateTurret()
+    {
+        Instantiate(turretObject, new Vector2(transform.position.x + 5f, transform.position.y), Quaternion.identity);
+    }
+}
